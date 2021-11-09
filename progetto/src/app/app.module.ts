2d59b319a56ee1461d62ccf8bb3ba0d79123f779
Vincenzo Coniglio <WhiteBit@users.noreply.github.com>
2021-11-09 11:19:00 +0100

    creazione componente

--- progetto/src/app/app.module.ts
+++ progetto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimoComponent } from './primo/primo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimoComponent
   ],
   imports: [
     BrowserModule,
